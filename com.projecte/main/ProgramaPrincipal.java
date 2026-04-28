bc6f25eb228c2105ccd87aac347fdec64399faf5
CesarOltraPart <cesoltpar@alu.edu.gva.es>
2026-04-28 18:53:55 +0200

    Sistema de login per fitxers

--- com.projecte/main/ProgramaPrincipal.java
+++ com.projecte/main/ProgramaPrincipal.java
@@ -4,101 +4,115 @@ import Alejandro.Alejandro;
 import Cesar.Cesar;
 import Daniel.Daniel;
 
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ProgramaPrincipal {
     static Scanner sc = new Scanner(System.in);
+    static Usuari usuariActiu = null;
+    private static File dirUsuaris = new File("./Usuaris");
+    private static File llistes = new File("./Llistes");
 
     public static void main(String[] args) throws ClassNotFoundException {
-        /*
-        falta fer la opcio de registre o login 
-        de momen gastem soles objected i ya farem
-        gestio de archius
-        */
-       ArrayList<Usuari> usuaris = new ArrayList<>();
-       Alejandro alejandro = new Alejandro("Alejandro");
-       Cesar cesar = new Cesar("Cesar");
-       Daniel daniel = new Daniel("Daniel");
-        Usuari usuariActiu = null;
-       alejandro.mostrarAlumne();
-       cesar.mostrarAlumne();
-       daniel.mostrarAlumne();
+        Alejandro alejandro = new Alejandro("Alejandro");
+        Cesar cesar = new Cesar("Cesar");
+        Daniel daniel = new Daniel("Daniel");
 
-       Pelicula p1 = new Pelicula("Star Wars Ep VII", "JJ. Abrams", "Mark Hamill");
+        alejandro.mostrarAlumne();
+        cesar.mostrarAlumne();
+        daniel.mostrarAlumne();
 
-       p1.inserirPelicules();
+        Pelicula p1 = new Pelicula("Star Wars Ep VII", "JJ. Abrams", "Mark Hamill");
+
+        p1.inserirPelicules();
         boolean eixir = false;
-       while (!eixir) {//temporal, heu organizarem cuant funcione tot be
-        System.out.println("1.registre");
-        System.out.println("2.inici de sesio");
-        System.out.println("3.Eixir");
-        int opcio = sc.nextInt();
-        sc.nextLine();//buffer
-        switch (opcio) {
-            case 1:
-                System.out.println("nom");
-                String nom = sc.nextLine();
+        while (!eixir) {// temporal, heu organizarem cuant funcione tot be
+            System.out.println("1.registre");
+            System.out.println("2.inici de sesio");
+            System.out.println("3.Eixir");
+            int opcio = sc.nextInt();
+            sc.nextLine();// buffer
+            switch (opcio) {
+                case 1: // TODO: deuria de comprobar si el nom ya existeix en usuaris antes de dixarte crearlo
+                    System.out.println("nom");
+                    String nom = sc.nextLine();
 
-                System.out.println("cognoms");
+                    System.out.println("cognoms");
 
-                String cogn = sc.nextLine();
-                System.out.println("correu");
+                    String cogn = sc.nextLine();
+                    System.out.println("correu");
 
-                String correu = sc.nextLine();
-                System.out.println("Contrasenya");
+                    String correu = sc.nextLine();
+                    System.out.println("Contrasenya");
 
-                String pwd = sc.nextLine();
-                System.out.println("Poblacio");
+                    String pwd = sc.nextLine();
+                    System.out.println("Poblacio");
 
-                String pob = sc.nextLine();
-                System.out.println("Data (dd/MM/yyyy)");
-                DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                LocalDate data = LocalDate.parse(sc.nextLine(), format);
-                usuaris.add(new Usuari(nom, cogn, correu, pwd, pob, data));
-                System.out.println("usuari creat, inicia sesio");
-                break;
-            case 2:
-                System.out.println("Nom de usuari");
-                String nomUs = sc.nextLine();
-                for (int i = 0; i < usuaris.size() && usuariActiu == null; i++) {
-                    if (usuaris.get(i).getNom().equals(nomUs)) {
-                        usuariActiu = usuaris.get(i);
-                    }   
-                }
-                if (usuariActiu == null) {
-                    System.out.println("Usuari no trobat");
+                    String pob = sc.nextLine();
+                    System.out.println("Data (dd/MM/yyyy)");
+                    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                    LocalDate data = LocalDate.parse(sc.nextLine(), format);
+                    usuariActiu = new Usuari(nom, cogn, correu, pwd, pob, data);
+                    System.out.println("usuari creat, inicia sesio");
                     break;
+                case 2:
+                    iniciSessio();
+                    break;
+                case 3:
+                    eixir = true;
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    static void iniciSessio() {
+        System.out.println("Nom de usuari");
+        String nomUs = sc.nextLine();
+
+        File[] llista = dirUsuaris.listFiles();
+        for (File file : llista) { //llig tots els objectes de la carpeta Usuaris i comproba si el nom de alguno coincidix en el nom introduit, si ho fa el estableix com a usuari actiu
+            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
+                Usuari us = (Usuari) in.readObject();
+                if (us.getNom().equals(nomUs)) {
+                    usuariActiu = us;
                 }
-                System.out.println("Contrasenya");
-                boolean fiInici = false;
-                do {//TODO: no funciona, no ix del bucle per algun motiu que desconec
-                    
-                    int intents = 0;
-                    String pwdUs = sc.nextLine();
-                    if (pwdUs.equals(usuariActiu.getPassword())) {
-                        System.out.println("Benvingut, " + usuariActiu.getNom());
-                    } else if (intents >= 3) {// no se ni si fa falta, es mes que res per a poder eixir de inici de sessio sense inicar sessio
-                        System.out.println("Massa intents fallits");
-                        usuariActiu = null;
-                        fiInici = true;
-                    } else {
-                        System.out.println("Contrasenya incorrecta, torna a intentar");
-                        intents++;
-                    }
-                } while (!fiInici);
-                
-                break;
-            case 3:
-                eixir = true;
-                break;
-            default:
-                break;
+            } catch (ClassNotFoundException e) {
+                System.out.println("Error, classe no trobada: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Error al llegir el fitxer de usuari: " + e.getMessage());
+            }
         }
 
-       }
+        if (usuariActiu == null) {
+            System.out.println("Usuari no trobat");
+            return;
+        }
+        System.out.println("Contrasenya");
+        boolean fiInici = false;
+        int intents = 0;
+        do {
+            String pwdUs = sc.nextLine();
+            if (pwdUs.equals(usuariActiu.getPassword())) {
+                System.out.println("Benvingut, " + usuariActiu.getNom());
+                fiInici = true;
+            } else if (intents >= 3) {// no se ni si fa falta, es mes que res per a poder eixir de inici de sessio
+                                      // sense inicar sessio
+                System.out.println("Massa intents fallits");
+                usuariActiu = null;
+                fiInici = true;
+            } else {
+                System.out.println("Contrasenya incorrecta, torna a intentar");
+                intents++;
+            }
+        } while (!fiInici);
     }
+
 }
--- com.projecte/main/Usuari.java
+++ com.projecte/main/Usuari.java
@@ -1,8 +1,13 @@
 package main;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Usuari {
+public class Usuari implements Serializable{
     
     //Variables de instancia
     private String nom;
@@ -12,10 +17,14 @@ public class Usuari {
     private String poblacio;
     private Rol rolsUsuari;
     private LocalDate data;
+    private int id;
+
+    private static File usuaris = new File("./Usuaris");
+    private static File llistes = new File("./Llistes");
+    private static int idLliure = llistes.listFiles().length -1;
 
     //Constructor
-    public Usuari(String nom, String cognoms, String correu, String password, String poblacio,
-            LocalDate data) {
+    public Usuari(String nom, String cognoms, String correu, String password, String poblacio, LocalDate data) {
         this.nom = nom;
         this.cognoms = cognoms;
         this.correu = correu;
@@ -23,6 +32,17 @@ public class Usuari {
         this.poblacio = poblacio;
         this.rolsUsuari = Rol.ROL_USUARI;//sols poden haber usuaris, el admin el creem amb antelacio
         this.data = data;
+        this.id = idLliure;
+        File usuari = new File(usuaris + "/" + this.nom + ".dat");
+        try (ObjectOutputStream ou = new ObjectOutputStream(new FileOutputStream(usuari))) {
+            ou.writeObject(this);
+            File carpetaPersonal = new File(llistes + "/" + this.id + this.correu.split("@")[0]);
+            carpetaPersonal.mkdir();
+        } catch (IOException e) {
+            System.out.println("Error creant el usuari: " + e.getMessage());
+        }
+        idLliure++;
+
     }
 
     //Getters - Setters
@@ -81,6 +101,14 @@ public class Usuari {
 
     public void setData(LocalDate data) {
         this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
    
